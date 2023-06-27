07811cacfac03d0371122c6fa2bf09a3dfaf5c27
Maxander <maxander77@gmail.com>
2023-06-27 16:07:02 +0200

    Initial commit

---
+++ public/loginPage.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const { response } = require("express");
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = data => {
+  alert(JSON.stringify(data));
+
+  ApiConnector.login(data, response => {
+    alert(JSON.stringify(response));
+
+    if (response.success) {
+      location.reload();
+    } else {
+      userForm.setLoginErrorMessage(response.error);
+     }
+   });
+  };
+  
+
+  userForm.registerFormCallback = data => {
+    alert(JSON.stringify(data));
+    
+    ApiConnector.register(data, response => {
+      alert(JSON.stringify(response));
+
+      if (response.success) {
+        location.reload();
+      } else {
+        userForm.setLoginErrorMessage(response.error);
+       }
+    });
+  };
+
+
